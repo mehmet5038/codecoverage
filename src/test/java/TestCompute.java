0140209986cd6acdfec0af07c2957a368ab9edc2
mehmet5038 <mkaratekin@etu.edu.tr>
2024-03-31 01:08:07 +0300

    covered Compute.java

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -1,17 +1,38 @@
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCompute {
-  Compute c;
+    private Compute c;
+    private MessageQueue mq;
 
-  @Test
-  public void example() {
-    MessageQueue mq = mock(MessageQueue.class);
-    c = new Compute(mq);
-    assertTrue(true);
-  }
+    @Before
+    public void setUp() {
+        mq = mock(MessageQueue.class);
+        c = new Compute(mq);
+    }
+    @Test
+    public void test_zero_length() {
+        when(mq.size()).thenReturn(0);
+        assertEquals(-1, c.countNumberOfOccurrences("208OpelAstraH1.3CDTI"));
+    }
+    @Test
+    public void testElementNotInQueue() {
+        when(mq.size()).thenReturn(10);
+        when(mq.contains("1998NissanSkylineGTR")).thenReturn(false);
+        assertEquals(0, c.countNumberOfOccurrences("2000FordFiesta1.25"));
+    }
+    @Test
+    public void testElementOccursMultipleTimes() {
+        when(mq.size()).thenReturn(6);
+        when(mq.contains("car")).thenReturn(true);
+        when(mq.getAt(0)).thenReturn("car");
+        when(mq.getAt(1)).thenReturn("tank");
+        when(mq.getAt(2)).thenReturn("car");
+        when(mq.getAt(3)).thenReturn("plane");
+        when(mq.getAt(4)).thenReturn("car");
+        when(mq.getAt(5)).thenReturn("car");
+        assertEquals(4, c.countNumberOfOccurrences("car"));
+    }
 }
